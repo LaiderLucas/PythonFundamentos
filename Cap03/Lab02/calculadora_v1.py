b59fd281eb4db758739db23610d9764d4594c4b2
Laider Lucas <laiderlucas@hotmail.com>
2020-01-14 21:49:02 -0400

    update

--- Cap03/Lab02/calculadora_v1.py
+++ Cap03/Lab02/calculadora_v1.py
@@ -6,3 +6,31 @@
 
 print("\n******************* Python Calculator *******************")
 
+def soma(x,y):
+    result = x + y
+    print(result)
+
+def subtracao(x,y):
+    result = x - y
+    print(result)
+
+def multiplicacao(x,y):
+    result = x * y
+    print(result)
+
+def divisao(x,y):
+    result = x/y
+    print(result)
+
+x = float(input("Informe o Primeiro Valor:"))
+y = float(input("Informe o Segundo Valor: "))
+op = str(input("Informe a operação: Multiplicação(X), Divisão(/), Soma(+), Subtração(-)"))
+
+if (op == "X"):
+    multiplicacao(x,y)
+elif(op == "/"):
+    divisao(x,y)
+elif(op == "+"):
+    soma(x,y)
+elif(op == "-"):
+    subtracao(x,y)
